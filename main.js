87fd837bbcbaa9ffbbcc6fcc8375d9013196d413
Juan Miguel Devilleros Ospina <juandevilleros@gmail.com>
2022-04-28 07:58:32 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("hi");
+document.querySelector(".botonMenu").addEventListener("click",()=> {
+    document.querySelector(".navMenu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showCase');
+ScrollReveal().reveal('.newsCards',{ delay:500});
+ScrollReveal().reveal('.cardsBannerOne',{ delay:500});
+ScrollReveal().reveal('.cardsBannerTwo',{ delay:500});
